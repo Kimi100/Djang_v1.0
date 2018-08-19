5ac233c47f9eda4c51e5fe44a71cf33396f0176c
xwp <xwp_fullstack@163.com>
2018-08-19 15:47:53 +0800

    设置和修改配置

---
+++ demo/libs/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 18-8-19 下午3:36
+# @Author  : kimi_ming
+# @File    : __init__.py.py
---
+++ demo/script/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 18-8-19 下午3:37
+# @Author  : kimi_ming
+# @File    : __init__.py.py
